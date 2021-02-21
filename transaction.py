549dae71ecb0546f7436a9d237a0bf7a4c1ff1b5
Ben Blute <benblute@gmail.com>
2021-02-20 21:09:09 -0500

    Modified the transaction class to use inputs and outputs

--- transaction.py
+++ transaction.py
@@ -6,53 +6,121 @@ from crypto import sha256, key_to_string, sign_message
 class Transaction:
 
     @classmethod
-    def create(cls, key, recipient, amount):
-        transaction = cls()
+    def create(cls, inputs, outputs):
+        obj = cls()
 
-        transaction.sender = key_to_string(key.public_key())
-        transaction.recipient = recipient
-        transaction.amount = amount
-        transaction.timestamp = str(time())
+        obj.inputs = inputs
+        obj.outputs = outputs
+        obj.timestamp = str(time())
 
-        string = transaction.sender
-        string += transaction.recipient
-        string += transaction.amount
-        string += transaction.timestamp
-
-        transaction.signature = sign_message(key, string)
-
-        return transaction
+        return obj
 
     @classmethod
     def from_json(cls, json):
-        transaction = cls()
+        obj = cls()
 
-        transaction.sender = json['sender']
-        transaction.recipient = json['recipient']
-        transaction.amount = json['amount']
-        transaction.timestamp = json['timestamp']
-        transaction.signature = json['signature']
+        obj.inputs = [Input.from_json(i) for i in json['inputs']]
+        obj.outputs = [Output.from_json(o) for o in json['outputs']]
+        obj.timestamp = json['timestamp']
 
-        return transaction
+        return obj
 
     def to_json(self):
         return {
-            'sender': self.sender,
-            'recipient': self.recipient,
-            'amount': self.amount,
-            'timestamp': self.timestamp,
-            'signature': self.signature
+            'inputs': [i.to_json() for i in self.inputs],
+            'outputs': [o.to_json() for o in self.outputs],
+            'timestamp': self.timestamp
         }
 
     def hash(self):
-        string = self.sender
-        string += self.recipient
-        string += self.amount
+        string = ''
+        for i in self.inputs:
+            string += i.hash()
+        for o in self.outputs:
+            string += o.hash()
         string += self.timestamp
-        string += self.signature
 
         return sha256(string)
 
     def __eq__(self, other):
         return self.hash() == other.hash()
+
+
+class Input:
+
+    @classmethod
+    def create(cls, key, block_hash, transaction_hash, output_hash):
+        obj = cls()
+
+        obj.block_hash = block_hash
+        obj.transaction_hash = transaction_hash
+        obj.output_hash = output_hash
+
+        string = obj.block_hash
+        string += obj.transaction_hash
+        string += obj.output_hash
+
+        obj.signature = sign_message(key, string)
+
+        return obj
+
+    @classmethod
+    def from_json(cls, json):
+        obj = cls()
+
+        obj.block_hash = json['block_hash']
+        obj.transaction_hash = json['transaction_hash']
+        obj.output_hash = json['output_hash']
+        obj.signature = json['signature']
+
+        return obj
+
+    def to_json(self):
+        return {
+            'block_hash': self.block_hash,
+            'transaction_hash': self.transaction_hash,
+            'output_hash': self.output_hash,
+            'signature': self.signature
+        }
+
+    def hash(self):
+        string = self.block_hash
+        string += self.transaction_hash
+        string += self.output_hash
+        string += self.signature
+
+        return sha256(string)
+
+
+class Output:
+
+    @classmethod
+    def create(cls, recipient, amount):
+        obj = cls()
+
+        obj.recipient = recipient
+        obj.amount = amount
+
+        return obj
+
+    @classmethod
+    def from_json(cls, json):
+        obj = cls()
+
+        obj.recipient = json['recipient']
+        obj.amount = json['amount']
+
+        return obj
+
+    def to_json(self):
+        return {
+            'recipient': self.recipient,
+            'amount': self.amount
+        }
+
+    def hash(self):
+        string = self.recipient
+        string += self.amount
+
+        return sha256(string)
 
